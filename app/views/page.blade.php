6ac5d9c0121c1df51ae79917ae81c1f68c622929
Adam Engebretson <adam@enge.me>
2013-11-01 16:04:26 -0500

    Add Contents Sidebar

--- app/views/page.blade.php
+++ app/views/page.blade.php
@@ -4,7 +4,20 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-sm-12" id="pageContent">
+      <div class="col-sm-3">
+        <h3>Contents</h3>
+        <ul>
+          <li><a href="#top">{{$page->get('doc.title')->asText()}}</a></li>
+      @foreach($page->get('doc.content')->getBlocks() as $block)
+        @if($block instanceof \Prismic\Fragment\Block\HeadingBlock)
+          <li style="margin-left: {{$block->getLevel()*15-30}}px;">
+            <a href="#{{str_replace('`', '', $block->getText())}}">{{preg_replace('/\`(.*?)\`/', '<code>$1</code>', $block->getText())}}</a>
+          </li>
+        @endif
+      @endforeach
+        </ul>
+      </div>
+      <div class="col-sm-9" id="pageContent">
         <h2>{{$page->get('doc.title')->asText()}}</h2>
         
         {{preg_replace('/\`(.*?)\`/', '<code>$1</code>', $page->get('doc.content')->asHtml())}}
@@ -18,8 +31,11 @@
   <script>
   $(function()
   {
+    $("h1, h2, h3", $("#pageContent")).prepend(function() {
+      return $("<a/>").attr('name', $(this).text().replace('<code>', '').replace('</code>', ''));
+    });
     $("#pageContent pre").addClass('prettyprint linenums');
-    $("#pageContent a").attr('target', '_blank').append(" ").append($("<span/>").addClass('fa fa-external-link'));
+    $("#pageContent a[href]").attr('target', '_blank').append(" ").append($("<span/>").addClass('fa fa-external-link'));
   });
   </script>
 @stop
